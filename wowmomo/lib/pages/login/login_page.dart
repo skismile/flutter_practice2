c18903310d3ebf2553936e4509921770a050e33e
SK ISMILE <101392872+skismile@users.noreply.github.com>
2023-02-08 13:00:15 +0530

    add

--- wowmomo/lib/pages/login/login_page.dart
+++ wowmomo/lib/pages/login/login_page.dart
@@ -25,12 +25,13 @@ class LoginPage extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
+                    children: const [
                       CircleAvatar(
-                        radius: 50,
-                        backgroundImage: NetworkImage(
-                            "https://vrchennai.com/UploadFile/StoreImage/wow-momo-th.jpg"),
-                      )
+                          radius: 50,
+                          backgroundImage: AssetImage("assets/images/logo.png")
+                          // NetworkImage(
+                          //     "https://vrchennai.com/UploadFile/StoreImage/wow-momo-th.jpg"),
+                          )
                     ],
                   )),
             ),
@@ -41,55 +42,121 @@ class LoginPage extends StatelessWidget {
               top: 250,
               left: 0,
               child: Container(
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(50),
                         topRight: Radius.circular(60))),
                 height: 500,
                 width: 400,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 70,
+                    ),
+                    Text(
+                      "#MoreWOWEveryday",
+                      style: TextStyle(
+                          color: Colors.amber[700],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30),
+                    ),
+                    RichText(
+                      text: TextSpan(
+                          // text: "Fulfill your",
+                          // style: TextStyle(color: Colors.black, fontSize: 30),
+                          children: [
+                            const TextSpan(
+                              text: " Fulfill your",
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 30),
+                            ),
+                            TextSpan(
+                              text: " cravings",
+                              style: TextStyle(
+                                  color: Colors.amber[700], fontSize: 30),
+                            ),
+                            TextSpan(
+                              text: " here!",
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 30),
+                            ),
+                          ]),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("Experience Health and Happiness in every"),
+                    Text("Order!"),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "Sign In / Register",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      width: 300,
+                      height: 50,
+                      child: TextField(
+                        keyboardType: TextInputType.numberWithOptions(),
+                        decoration: InputDecoration(
+                            hintText: '+91 7001727360',
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            )),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      width: 300,
+                      height: 50,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: StadiumBorder(),
+                              backgroundColor: Colors.amber[700]),
+                          onPressed: () {},
+                          child: Text("Next")),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("Or"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                     Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          backgroundImage:
+                              AssetImage("assets/images/google_logo.png"),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          backgroundImage:
+                              AssetImage("assets/images/threedot.png"),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           )
         ],
       ),
-    )
-
-        // Column(
-        //   children: [
-        //     Indexer(children: [
-        //       Indexed(
-        //           index: 1, //more the index, upper the order
-        //           child: Positioned(
-        //             top: 50,
-        //             left: 50,
-        //             child: Container(
-        //               width: double.infinity,
-        //               // height: 350,
-        //               color: Colors.amber[600],
-        //               child: Text("data"),
-        //             ),
-        //           )),
-        //       Indexed(
-        //           index: 2, //more the index, upper the order
-        //           child: Positioned(
-        //             top: 50,
-        //             left: 50,
-        //             child: Container(
-        //               decoration: BoxDecoration(
-        //                   color: Colors.red,
-        //                   borderRadius: BorderRadius.only(
-        //                       topLeft: Radius.circular(40),
-        //                       topRight: Radius.circular(40))),
-        //               width: double.infinity,
-        //               // height: 472,
-        //               child: const Text("data"),
-        //             ),
-        //           )),
-        //     ]),
-        //   ],
-        // )
-
-        );
+    ));
   }
 }
