f590e7f8585c9e754798313dfa2e4bfe374efd58
SK ISMILE <101392872+skismile@users.noreply.github.com>
2023-03-02 19:04:28 +0530

    add

--- wowmomo/lib/main.dart
+++ wowmomo/lib/main.dart
@@ -13,6 +13,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    
     // TODO: implement build
     return GetMaterialApp(
       theme: ThemeData(
